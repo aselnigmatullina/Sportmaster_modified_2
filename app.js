95858c3123512d79751f6c2bfad6dbf8b6b12cc2
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-09-19 14:59:11 +0300

    code changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Домашнее задание 2: Спортмастер
-let purchaseAmount=3051;
-let minBonus=300;
-let bonus=(purchaseAmount-(purchaseAmount%minBonus))*0.1;
+const purchaseAmount=3051;
+const minBonus=300;
+const bonus=(purchaseAmount-(purchaseAmount%minBonus))*0.1;
 console.log(bonus);
